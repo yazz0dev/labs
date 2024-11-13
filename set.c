a94e0c42bbd7d80ec78abc26bc47cacb94e309c9
Yaseen k <181333791+yazz0dev@users.noreply.github.com>
2024-11-13 18:44:58 +0530

    delete

--- set.c
+++ set.c
@@ -3,10 +3,33 @@
 
 
 #define MAX_SET_SIZE 10
-int universal_set[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+int universal_set[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, SIZE=9;
 
 
-typedef unsigned char Set:1;
+// Function prototypes
+Set readset();
+void printset(Set s);
+Set set_union(Set a, Set b);
+Set set_intersection(Set a, Set b);
+Set set_difference(Set a, Set b);
+
+typedef struct {unsigned char x:1} bit;
+
+void main()
+{
+    bit a[10]={0}, b[10]={0},c[10]={0};
+
+    int n;
+    
+    printset(set_union(setA, setB));
+    printset(set_intersection(setA, setB));
+    printset(set_difference(setA, setB));
+    printf("Set A: ");
+    printset(setA);
+    printf("Set B: ");
+    printset(setB);
+}
+
 
 // Function to print a set
 void printset(Set s)
@@ -42,16 +65,3 @@ Set set_difference(Set a, Set b)
   return a & b;
 }
 
-int main()
-{
-    Set setA, setB;
-    setA = readset();
-    setB = readset();
-    printset(set_union(setA, setB));
-    printset(set_intersection(setA, setB));
-    printset(set_difference(setA, setB));
-    printf("Set A: ");
-    printset(setA);
-    printf("Set B: ");
-    printset(setB);
-}
